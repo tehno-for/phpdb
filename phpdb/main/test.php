1350bf2c16602a65b3bf90398c6573cd82f32aed
keoroot <keoroot@gmail.com>
2021-10-08 01:46:07 +0400

    test file

--- phpdb/main/test.php
+++ phpdb/main/test.php
@@ -1,17 +1,14 @@
-{ 
-    [0]=> array(4) { 
-        ["member_id"]=> string(1) "1" 
-        ["email"]=> string(23) "randy@teamtreehouse.com" 
-        ["fullname"]=> string(10) "Randy Hoyt" 
-        ["level"]=> string(6) "bronze" } 
-    [1]=> array(4) { 
-        ["member_id"]=> string(1) "2" 
-        ["email"]=> string(25) "hampton@teamtreehouse.com" 
-        ["fullname"]=> string(13) "Hampton Paulk" 
-        ["level"]=> string(6) "silver" } 
-    [2]=> array(4) { 
-        ["member_id"]=> string(1) "3" 
-        ["email"]=> string(23) "alena@teamtreehouse.com" 
-        ["fullname"]=> string(14) "Alena Holligan" 
-        ["level"]=> string(4) "gold" } 
-} 
+<?php
+$letters = ['A', 'C', 'M', 'E'];
+
+
+echo "Today's challenge is brought to you by the ";
+echo count($letters);
+echo " letters: "; 
+foreach($letters as $v){
+   $m = $v;
+};
+echo $m;
+echo " .";
+
+?>
